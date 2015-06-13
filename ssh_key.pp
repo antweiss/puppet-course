aac7881c738592b1043c95b35aef5d346b7df16e
Anton Weiss <anton.weiss@primarydata.com>
2015-06-13 12:17:16 +0300

    ssh key manifest

---
+++ ssh_key.pp
@@ -0,0 +1,5 @@
+ssh_authorized_key{ 'root@puppet-agent.otomato.com':
+  user => 'root',
+  type => 'ssh-rsa',
+  key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDfgQzLrHsMVp35d6AVtH224aWQ0XGS+3fx1SSCwehFbEBqEVtaIGxTZLwGSqPgrrnUDjg7hcZJv7JX+r7Yd1KYhJXNgE4qGZAePrELRJdaBXVRgCdkmenNMEjywprulFU7aZ4Cb+bxaaJF1EUwZ+WHrO1HT+yXCnpPAL6ieG673/Z1w0D0NL8AzYw0IsBoXjd0kof19eJaFTuZ1K84QNxKaFgatbgpWN/5jthzuKikkAAEBNPAt9V4A5W/SKwMZFSlB0oILRbUaWpfDn7iUooytHI6ZL9osrGa6rf1zdv3uEx3EhIncZl1oegIgstd3cTH2P5qFZ4atL4ACiw+mB8t'
+}
